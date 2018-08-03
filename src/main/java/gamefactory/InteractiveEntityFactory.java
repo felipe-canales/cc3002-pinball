f6697de9fd6c5a16ef202b0103094fea877b6489
felipe-canales <felipe.canales.213@gmail.com>
2018-08-03 15:00:32 -0400

    Refactorized duplicated code in InteractiveEntityFactory of newBumper and newTarget in newHittable

--- src/main/java/gamefactory/InteractiveEntityFactory.java
+++ src/main/java/gamefactory/InteractiveEntityFactory.java
@@ -2,6 +2,7 @@ package gamefactory;
 
 import com.almasb.fxgl.entity.Entities;
 import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.component.Component;
 import com.almasb.fxgl.entity.components.CollidableComponent;
 import com.almasb.fxgl.physics.BoundingShape;
 import com.almasb.fxgl.physics.HitBox;
@@ -9,11 +10,13 @@ import com.almasb.fxgl.physics.PhysicsComponent;
 import com.almasb.fxgl.physics.box2d.dynamics.BodyType;
 import com.almasb.fxgl.physics.box2d.dynamics.FixtureDef;
 import component.BumperComponent;
+import component.HittableComponent;
 import component.TargetComponent;
 import entitytype.EntityType;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import logic.gameelements.Hittable;
 import logic.gameelements.bumper.Bumper;
 import logic.gameelements.target.Target;
 import visitor.ShapePicker;
@@ -46,7 +49,7 @@ public class InteractiveEntityFactory {
     public static Entity newBall() {
         PhysicsComponent physics = new PhysicsComponent();
         physics.setBodyType(BodyType.DYNAMIC);
-        physics.setFixtureDef(new FixtureDef().restitution(0.8f).density(0.1f));
+        physics.setFixtureDef(new FixtureDef().restitution(0.9f).density(0.1f));
         physics.setOnPhysicsInitialized(() -> physics.setLinearVelocity(0, -5 * 200));
 
         return Entities.builder()
@@ -59,20 +62,19 @@ public class InteractiveEntityFactory {
     }
 
     public static Entity newBumper(int x, int y, Bumper b) {
-        return Entities.builder()
-                .at(x, y)
-                .type(EntityType.BUMPER)
-                .viewFromNodeWithBBox(new ShapePicker(b).getShape())
-                .with(new PhysicsComponent(), new BumperComponent(b), new CollidableComponent(true))
-                .build();
+        return newHittable(x, y, b, new BumperComponent(b), EntityType.BUMPER);
     }
 
     public static Entity newTarget(int x, int y, Target t) {
+        return newHittable(x, y, t, new TargetComponent(t), EntityType.TARGET);
+    }
+
+    private static Entity newHittable(int x, int y, Hittable h, HittableComponent hc, EntityType type) {
         return Entities.builder()
                 .at(x, y)
-                .type(EntityType.TARGET)
-                .viewFromNodeWithBBox(new ShapePicker(t).getShape())
-                .with(new PhysicsComponent(), new TargetComponent(t), new CollidableComponent(true))
+                .type(type)
+                .viewFromNodeWithBBox(new ShapePicker(h).getShape())
+                .with(new PhysicsComponent(), new CollidableComponent(true), (Component) hc)
                 .build();
     }
 }
